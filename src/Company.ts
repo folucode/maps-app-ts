188a8b37f9a6a57d7eb98405c74ae1d87142dd94
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-11-09 18:40:24 +0100

    🎉 Initial commit

---
+++ src/Company.ts
@@ -0,0 +1,25 @@
+import faker from "faker";
+
+export class Company {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+        <h4>${this.name} is a company with the catch phrase of ${this.catchPhrase}...</h4
+      `;
+  }
+}
---
+++ src/User.ts
@@ -0,0 +1,24 @@
+import faker from "faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+      <h4>This is the location of ${this.name}...</h4>
+    `;
+  }
+}
